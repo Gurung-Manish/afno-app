46409798f4e788eed959a0cf6d1a83fb259e9a76
Aayurt <aayurtshrestha@gmail.com>
2023-10-01 06:28:18 +0100

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:afno_app/bloc_provider.dart';
 import 'package:afno_app/core/service_locator/service_locator.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
-  setPathUrlStrategy();
-
   String envFile = '.env.development';
   await dotenv.load(fileName: envFile);
   await setUpServiceLocator();
+  setPathUrlStrategy();
+
   runApp(const BlocProviderContainer());
 }
